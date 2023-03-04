8a1ab47bd416dbc32c96bf5c6956a4ec21252cf9
Akiomi Kamakura <akiomik@gmail.com>
2023-03-05 03:19:05 +0900

    Add icons to posts

--- app/src/main/java/io/github/akiomik/seiun/MainActivity.kt
+++ app/src/main/java/io/github/akiomik/seiun/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.ChatBubbleOutline
+import androidx.compose.material.icons.sharp.FavoriteBorder
+import androidx.compose.material.icons.sharp.SyncAlt
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TabRowDefaults.Divider
@@ -17,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
@@ -39,55 +45,148 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun RetweetText(viewPost: FeedViewPost) {
+    Box(modifier = Modifier.padding(bottom = 8.dp)) {
+        Text(
+            text = "Reposted by ${viewPost.reason?.by?.displayName}",
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray,
+            style = MaterialTheme.typography.labelMedium
+        )
+    }
+}
+
+@Composable
+fun ReplyText(viewPost: FeedViewPost) {
+    Box(modifier = Modifier.padding(bottom = 8.dp)) {
+        Text(
+            text = "Replying to ${viewPost.reply?.parent?.author?.displayName}",
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray,
+            style = MaterialTheme.typography.labelMedium
+        )
+    }
+}
+
+@Composable
+fun Avatar(viewPost: FeedViewPost) {
+    AsyncImage(
+        model = viewPost.post.author.avatar,
+        contentDescription = null,
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+fun DisplayName(viewPost: FeedViewPost) {
+    Text(
+        text = "${viewPost.post.author.displayName}",
+        style = MaterialTheme.typography.titleMedium,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun Handle(viewPost: FeedViewPost) {
+    Text(
+        text = "@${viewPost.post.author.handle}",
+        style = MaterialTheme.typography.labelMedium,
+        color = Color.Gray
+    )
+}
+
+@Composable
+fun NameRow(viewPost: FeedViewPost) {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        DisplayName(viewPost = viewPost)
+        Handle(viewPost = viewPost)
+    }
+}
+
+@Composable
+fun ReplyIndicator(viewPost: FeedViewPost) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            painter = rememberVectorPainter(Icons.Sharp.ChatBubbleOutline),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+            tint = Color.Gray
+        )
+        Text(text = viewPost.post.replyCount.toString(), color = Color.Gray)
+    }
+}
+
+@Composable
+fun RepostIndicator(viewPost: FeedViewPost) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            painter = rememberVectorPainter(Icons.Sharp.SyncAlt),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+            tint = Color.Gray
+        )
+        Text(text = viewPost.post.repostCount.toString(), color = Color.Gray)
+    }
+}
+
+@Composable
+fun UpvoteIndicator(viewPost: FeedViewPost) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            painter = rememberVectorPainter(Icons.Sharp.FavoriteBorder),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+            tint = Color.Gray
+        )
+        Text(text = viewPost.post.upvoteCount.toString(), color = Color.Gray)
+    }
+}
+
+@Composable
+fun FeedPostContent(viewPost: FeedViewPost) {
+    Column(modifier = Modifier.padding(start = 8.dp)) {
+        NameRow(viewPost = viewPost)
+        Text(text = viewPost.post.record.text)
+        Row(
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            ReplyIndicator(viewPost = viewPost)
+            RepostIndicator(viewPost = viewPost)
+            UpvoteIndicator(viewPost = viewPost)
+        }
+    }
+}
+
+@Composable
 fun FeedPost(viewPost: FeedViewPost) {
     Column(modifier = Modifier.padding(10.dp)) {
         if (viewPost.reason?.type == "app.bsky.feed.feedViewPost#reasonRepost")  {
-            Box(modifier = Modifier.padding(bottom = 8.dp)) {
-                Text(
-                    text = "Reposted by ${viewPost.reason.by?.displayName}",
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Gray,
-                    style = MaterialTheme.typography.labelMedium
-                )
-            }
+            RetweetText(viewPost = viewPost)
         }
         else if (viewPost.reply != null) {
-            Box(modifier = Modifier.padding(bottom = 8.dp)) {
-                Text(
-                    text = "Replying to ${viewPost.reply.parent.author.displayName}",
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Gray,
-                    style = MaterialTheme.typography.labelMedium
-                )
-            }
+            ReplyText(viewPost = viewPost)
         }
+
         Row {
-            AsyncImage(
-                model = viewPost.post.author.avatar,
-                contentDescription = null,
-                modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .clip(CircleShape)
-            )
-            Column(modifier = Modifier.padding(start = 8.dp)) {
-                Row(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "${viewPost.post.author.displayName}",
-                        style = MaterialTheme.typography.titleMedium,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = "@${viewPost.post.author.handle}",
-                        style = MaterialTheme.typography.labelMedium,
-                        color = Color.Gray
-                    )
-                }
-                Text(text = viewPost.post.record.text)
-            }
+            Avatar(viewPost = viewPost)
+            FeedPostContent(viewPost = viewPost)
         }
     }
 }
