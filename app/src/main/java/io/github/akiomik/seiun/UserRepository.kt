94819532037263b6b05c922e9116cf7ef72106f2
Akiomi Kamakura <akiomik@gmail.com>
2023-03-04 20:38:16 +0900

    Add UserRepository

---
+++ app/src/main/java/io/github/akiomik/seiun/UserRepository.kt
@@ -0,0 +1,28 @@
+package io.github.akiomik.seiun
+
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.example.catpaw.models.Session
+
+class UserRepository(context: Context) {
+    private val key = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val sharedPreferences = EncryptedSharedPreferences.create(
+        context,
+        "seiun",
+        key,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
+
+    fun getSession(): Session {
+        val accessJwt = sharedPreferences.getString("accessJwt", "") ?: ""
+        val refreshJwt = sharedPreferences.getString("refreshJwt", "") ?: ""
+        val handle = sharedPreferences.getString("handle", "") ?: ""
+        val did = sharedPreferences.getString("did", "") ?: ""
+
+        return Session(accessJwt = accessJwt, refreshJwt = refreshJwt, handle = handle, did = did)
+    }
+}
