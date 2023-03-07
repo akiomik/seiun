06830fbfae26a622ce9fc6783956da8e11a2c82e
Akiomi Kamakura <akiomik@gmail.com>
2023-03-08 00:10:10 +0900

    Add TopBar

--- app/src/main/java/io/github/akiomik/seiun/SeiunActivity.kt
+++ app/src/main/java/io/github/akiomik/seiun/SeiunActivity.kt
@@ -1,21 +1,23 @@
 package io.github.akiomik.seiun
 
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -24,12 +26,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import io.github.akiomik.seiun.model.Notification
 import io.github.akiomik.seiun.ui.login.LoginScreen
 import io.github.akiomik.seiun.ui.notification.NotificationScreen
 import io.github.akiomik.seiun.ui.registration.RegistrationScreen
 import io.github.akiomik.seiun.ui.theme.SeiunTheme
 import io.github.akiomik.seiun.ui.timeline.NewPostFab
 import io.github.akiomik.seiun.ui.timeline.TimelineScreen
+import io.github.akiomik.seiun.viewmodel.TimelineViewModel
 
 class SeiunActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +43,17 @@ class SeiunActivity : ComponentActivity() {
         setContent {
             App()
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(scrollBehavior: TopAppBarScrollBehavior, visible: MutableState<Boolean>) {
+    AnimatedVisibility(visible = visible.value) {
+        TopAppBar(
+            title = { Text("Seiun") },
+            scrollBehavior = scrollBehavior
+        )
     }
 }
 
@@ -77,7 +93,7 @@ fun BottomBar(navController: NavController, visible: MutableState<Boolean>) {
 
 @Composable
 fun Navigation(navController: NavHostController, modifier: Modifier) {
-    NavHost(navController = navController, startDestination = "login", modifier = Modifier) {
+    NavHost(navController = navController, startDestination = "login", modifier = modifier) {
         composable("timeline") { TimelineScreen(/*...*/) }
         composable("notification") { NotificationScreen(/*...*/) }
         composable("login") {
@@ -95,33 +111,44 @@ fun Navigation(navController: NavHostController, modifier: Modifier) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
     val navController = rememberNavController()
+    val topBarState = rememberSaveable { (mutableStateOf(false)) }
     val bottomBarState = rememberSaveable { (mutableStateOf(false)) }
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val fabState = remember { (mutableStateOf<@Composable () -> Unit>({})) }
     when (navBackStackEntry?.destination?.route) {
         "timeline" -> {
+            topBarState.value = true
             bottomBarState.value = true
             fabState.value = { NewPostFab() }
         }
         "notification" -> {
+            topBarState.value = true
             bottomBarState.value = true
             fabState.value = {}
         }
         else -> {
+            topBarState.value = false
             bottomBarState.value = false
             fabState.value = {}
         }
     }
 
+
     SeiunTheme {
         Scaffold(
+            topBar = { TopBar(scrollBehavior, visible = topBarState) },
             bottomBar = { BottomBar(navController = navController, visible = bottomBarState) },
-            floatingActionButton = fabState.value
-        ) {
-            Navigation(navController = navController, modifier = Modifier.padding(it))
-        }
+            floatingActionButton = fabState.value,
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            content = {
+                Navigation(navController = navController, modifier = Modifier.padding(it))
+            }
+        )
     }
 }
