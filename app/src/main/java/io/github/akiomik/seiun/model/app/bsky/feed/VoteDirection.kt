0f606ba40fe416fb8facc34c45a6810e13c98e8c
Akiomi Kamakura <akiomik@gmail.com>
2023-03-11 18:49:25 +0900

    Add CI workflow

--- app/src/main/java/io/github/akiomik/seiun/model/app/bsky/feed/VoteDirection.kt
+++ app/src/main/java/io/github/akiomik/seiun/model/app/bsky/feed/VoteDirection.kt
@@ -1,6 +1,8 @@
 package io.github.akiomik.seiun.model.app.bsky.feed
 
 // TODO: Use sealed interface
+/* ktlint-disable enum-entry-name-case */
 enum class VoteDirection {
     up, down, none
 }
+/* ktlint-enable enum-entry-name-case */
