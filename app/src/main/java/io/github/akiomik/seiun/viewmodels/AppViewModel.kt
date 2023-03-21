841e03f2512ebfea9b4483b7d7ccf32bc4935a28
Akiomi Kamakura <akiomik@gmail.com>
2023-03-21 21:28:32 +0900

    Make AppViewModel singleton

--- app/src/main/java/io/github/akiomik/seiun/viewmodels/AppViewModel.kt
+++ app/src/main/java/io/github/akiomik/seiun/viewmodels/AppViewModel.kt
@@ -2,7 +2,6 @@ package io.github.akiomik.seiun.viewmodels
 
 import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewModelScope
 import io.github.akiomik.seiun.SeiunApplication
 import io.github.akiomik.seiun.model.app.bsky.actor.Profile
@@ -13,62 +12,63 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 
-class AppViewModel : ApplicationViewModel() {
-    private val _atpService = SeiunApplication.instance!!.atpService
+object AppViewModel : ApplicationViewModel() {
+    private val innerAtpService = SeiunApplication.instance!!.atpService
 
-    private var _profile = MutableStateFlow<Profile?>(null)
-    private var _showDrawer = MutableStateFlow(false)
-    private var _showTopBar = MutableStateFlow(false)
-    private var _showBottomBar = MutableStateFlow(false)
-    private var _fab = MutableStateFlow<@Composable () -> Unit>({})
+    private var innerProfile = MutableStateFlow<Profile?>(null)
+    private var innerShowDrawer = MutableStateFlow(false)
+    private var innerShowTopBar = MutableStateFlow(false)
+    private var innerShowBottomBar = MutableStateFlow(false)
+    private var innerFab = MutableStateFlow<@Composable () -> Unit>({})
 
-    private val isDrawerAvailable = _atpService.combine(_profile) { atpService, profile ->
+    private val isDrawerAvailable = innerAtpService.combine(innerProfile) { atpService, profile ->
         atpService != null && profile != null
     }
 
-    val profile = _profile.asStateFlow()
-    val showDrawer = _showDrawer.combine(isDrawerAvailable) { showDrawer, isAvailable ->
+    val profile = innerProfile.asStateFlow()
+    val showDrawer = innerShowDrawer.combine(isDrawerAvailable) { showDrawer, isAvailable ->
         showDrawer && isAvailable
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
-    val showTopBar = _showTopBar.asStateFlow()
-    val showBottomBar = _showBottomBar.asStateFlow()
-    val fab = _fab.asStateFlow()
+    val showTopBar = innerShowTopBar.asStateFlow()
+    val showBottomBar = innerShowBottomBar.asStateFlow()
+    val fab = innerFab.asStateFlow()
 
     private val userRepository = SeiunApplication.instance!!.userRepository
 
     fun updateProfile() {
         wrapError(
             run = { userRepository.getProfile() },
-            onSuccess = { _profile.value = it },
+            onSuccess = { innerProfile.value = it },
             onError = { Log.d(SeiunApplication.TAG, "Failed to init ProfileViewModel: $it") }
         )
     }
 
     fun onTimeline() {
-        _showDrawer.value = true
-        _showTopBar.value = true
-        _showBottomBar.value = true
-        _fab.value = { NewPostFab() }
+        innerShowDrawer.value = true
+        innerShowTopBar.value = true
+        innerShowBottomBar.value = true
+        innerFab.value = { NewPostFab() }
     }
 
     fun onNotification() {
-        _showDrawer.value = true
-        _showTopBar.value = true
-        _showBottomBar.value = true
-        _fab.value = {}
+        innerShowDrawer.value = true
+        innerShowTopBar.value = true
+        innerShowBottomBar.value = true
+        innerFab.value = {}
     }
 
     fun onUser() {
-        _showDrawer.value = false
-        _showTopBar.value = false
-        _showBottomBar.value = true
-        _fab.value = {}
+        innerShowDrawer.value = false
+        innerShowTopBar.value = false
+        innerShowBottomBar.value = true
+        innerFab.value = {}
     }
 
     fun onLoginOrRegistration() {
-        _showDrawer.value = false
-        _showTopBar.value = false
-        _showBottomBar.value = false
-        _fab.value = {}
+        innerProfile.value = null
+        innerShowDrawer.value = false
+        innerShowTopBar.value = false
+        innerShowBottomBar.value = false
+        innerFab.value = {}
     }
 }
