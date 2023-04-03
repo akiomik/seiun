5d5325f4aa3913f8765ec5ddf7c3372f7759a85a
Akiomi Kamakura <akiomik@gmail.com>
2023-04-03 11:35:01 +0900

    Fix external

--- app/src/main/java/io/github/akiomik/seiun/model/app/bsky/embed/External.kt
+++
@@ -1,8 +0,0 @@
-package io.github.akiomik.seiun.model.app.bsky.embed
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class External(
-    val external: ExternalExternal? = null
-)
